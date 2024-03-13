a3110d8845f27289e05a176d0aecee6c060afb06
Oskaras-coder <osafinas@gmail.com>
2024-03-13 17:57:39 +0200

    Added new feature

--- HelloWorld2/src/main/java/org/example/Main.java
+++ HelloWorld2/src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Bla bla");
+        new_feature();
+    }
 
+    private static void new_feature(){
+        System.out.println("New feature");
     }
 }
