6cb9c80ad3da51fd0ad5945f35c03f77f669e6f6
Oskaras-coder <osafinas@gmail.com>
2024-03-13 16:41:00 +0200

    Bla bla

--- HelloWorld2/src/main/java/org/example/Main.java
+++ HelloWorld2/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Conflict!!!");
+        System.out.println("Bla bla");
     }
 }
